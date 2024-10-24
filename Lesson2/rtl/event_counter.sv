42c28687aca4e0d8a573c2c7f960648318ab5c60
DaoQuyet <nmhtdyt@gmail.com>
2024-10-25 01:06:16 +0700

    no message

--- Lesson2/rtl/event_counter.sv
+++ Lesson2/rtl/event_counter.sv
@@ -5,12 +5,9 @@ module event_counter (
     output reg [7:0] count = 0         //! Count the number of events that have occurred
 );
 
-  always @(posedge event_triggered or posedge rst) begin
-    if (rst) begin
-      count <= 0;
-    end else begin
-      if (!hold) count <= count + 1;
-    end
+  always @(posedge event_triggered, posedge rst) begin
+    if (rst) count <= 0;
+    else if (!hold) count <= count + 1;
   end
 
 endmodule
